bcfd32113c2752ebc07e8b4195035668a6e79abc
canto9112 <cantoweb112@gmail.com>
2021-05-19 22:55:50 +0300

    Добавил функции получения id картинки и обновления главной картинки товара

--- moltin/moltin.py
+++ moltin/moltin.py
@@ -1,4 +1,41 @@
 import requests
+from pprint import pprint
+
+
+def create_main_image(token, product_id, image_id):
+    headers = {'Authorization': f'Bearer {token}',
+               'Content-Type': 'application/json'}
+
+    data = {'data': {
+                "type": "main_image",
+                "id": image_id
+    }}
+
+    response = requests.post(f'https://api.moltin.com/v2/products/{product_id}/relationships/main-image',
+                             headers=headers, json=data)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_file_id(token, file_name, file_url):
+    response = requests.get(file_url)
+    response.raise_for_status()
+
+    with open(f'{file_name}.jpg', 'wb') as file:
+        file.write(response.content)
+
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    files = {
+        'public': True,
+        'file': open(f'{file_name}.jpg', 'rb')
+
+    }
+
+    response = requests.post('https://api.moltin.com/v2/files', headers=headers, files=files)
+    pprint(response.json())
+    return response.json()['data']['id']
 
 
 def get_image_url(token, file_id):
