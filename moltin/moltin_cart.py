44a9eda7b087d01b4eaeb1a2ae2215e8140223e3
canto9112 <cantoweb112@gmail.com>
2021-05-21 20:04:50 +0300

    Вынес функции добавления и удаления товара в moltin_cart.py

--- moltin/moltin_cart.py
+++ moltin/moltin_cart.py
@@ -27,3 +27,27 @@ def clean_cart(token, cart_id):
     }
     response = requests.delete(f'https://api.moltin.com/v2/carts/{cart_id}/items', headers=headers)
     response.raise_for_status()
+
+
+def add_product_to_cart(token, product_id, cart_name, quantity):
+    headers = {'Authorization': f'Bearer {token}',
+               'Content-Type': 'application/json'}
+
+    data = {'data': {'id': product_id,
+                     'type': 'cart_item',
+                     'quantity': int(quantity)}}
+
+    response = requests.post(f'https://api.moltin.com/v2/carts/{cart_name}/items',
+                             headers=headers,
+                             json=data)
+    response.raise_for_status()
+    return response.json()
+
+
+def delete_product_in_cart(token, cart_name, product_id):
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    response = requests.delete(f'https://api.moltin.com/v2/carts/{cart_name}/items/{product_id}', headers=headers)
+    response.raise_for_status()
+    return response.json()
--- moltin/moltin_product.py
+++ moltin/moltin_product.py
@@ -44,26 +44,3 @@ def get_all_products(token):
     response = response.json()
     return response['data']
 
-
-def add_product_to_cart(token, product_id, cart_name, quantity):
-    headers = {'Authorization': f'Bearer {token}',
-               'Content-Type': 'application/json'}
-
-    data = {'data': {'id': product_id,
-                     'type': 'cart_item',
-                     'quantity': int(quantity)}}
-
-    response = requests.post(f'https://api.moltin.com/v2/carts/{cart_name}/items',
-                             headers=headers,
-                             json=data)
-    response.raise_for_status()
-    return response.json()
-
-
-def delete_product_in_cart(token, cart_name, product_id):
-    headers = {
-        'Authorization': f'Bearer {token}'
-    }
-    response = requests.delete(f'https://api.moltin.com/v2/carts/{cart_name}/items/{product_id}', headers=headers)
-    response.raise_for_status()
-    return response.json()
