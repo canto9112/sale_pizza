2099fcb016af94f84ad958135cfa0049eb265978
canto9112 <cantoweb112@gmail.com>
2021-07-02 12:50:25 +0300

    Высушил код

--- telegram_pizza/payments.py
+++ telegram_pizza/payments.py
@@ -15,29 +15,22 @@ def start_with_shipping_callback(bot, update, chat_id, price):
 
 def shipping_callback(bot, update):
     query = update.shipping_query
-    # check the payload, is this from your bot?
     if query.invoice_payload != 'Custom-Payload':
-        # answer False pre_checkout_query
         bot.answer_shipping_query(shipping_query_id=query.id, ok=False,
                                   error_message="Something went wrong...")
         return
     else:
         options = list()
-        # a single LabeledPrice
         options.append(ShippingOption('1', 'Shipping Option A', [LabeledPrice('A', 100)]))
-        # an array of LabeledPrice objects
         price_list = [LabeledPrice('B1', 150), LabeledPrice('B2', 200)]
         options.append(ShippingOption('2', 'Shipping Option B', price_list))
-        bot.answer_shipping_query(shipping_query_id=query.id, ok=True,
-                                  shipping_options=options)
+        bot.answer_shipping_query(shipping_query_id=query.id, ok=True, shipping_options=options)
 
 
-# after (optional) shipping, it's the pre-checkout
 def precheckout_callback(bot, update):
     query = update.pre_checkout_query
-    # check the payload, is this from your bot?
+
     if query.invoice_payload != 'Custom-Payload':
-        # answer False pre_checkout_query
         bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=False,
                                       error_message="Something went wrong...")
     else:
